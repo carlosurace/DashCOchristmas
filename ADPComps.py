a96073c5154904784d72549039e2215b67374ade
Carlo Surace <carlosurace@gmail.com>
2021-07-27 16:21:48 -0500

    update

--- ADPComps.py
+++ ADPComps.py
@@ -72,34 +72,47 @@ statscolumns = [col for col in ADP.columns]
 '''
    
 ADPComps =html.Div([
-        html.H1(children='ADP Comps'),
+        html.H1(children='AOD Comparison Engine'),
         html.Div(id="wrapper",children=[
-                dcc.Dropdown(id = 'Player',value=None
+        dbc.Row([
+            dbc.Col([dbc.Label("Player")],width=1),
+        
+                dbc.Col([dcc.Dropdown(id = 'Player',value=None
                         ,options=[
                             {'label': i, 'value': i} for i in sorted(ADP.Player.unique())],
                         multi=False
                         ,className="dash-bootstrap"
-                    ),
-                dcc.Dropdown(id = 'Season',value=None
+                    )],width=11),
+        ]),
+        dbc.Row([
+            dbc.Col([dbc.Label("Year(Y1)")],width=1),
+                dbc.Col([dcc.Dropdown(id = 'Season',value=None
                         ,options=[
                             {'label': i, 'value': i} for i in sorted(ADP.Year.unique())],
                         multi=False
                         ,className="dash-bootstrap"
-                    ),
-                dcc.Dropdown(id = 'CompStats',
+                    )],width=11),
+        ]),
+        dbc.Row([
+            dbc.Col([dbc.Label("Comparison Stats")],width=1),
+                dbc.Col([dcc.Dropdown(id = 'CompStats',
                         options=[
                             {'label': i, 'value': i} for i in statscolumns],
                         multi=True
                         ,value=["POS.ADP","Y1 Finish"]
                         ,className="dash-bootstrap"
-                    ),
-                dcc.Dropdown(id = 'DispStats',
+                    )],width=11),
+        ]),
+        dbc.Row([
+            dbc.Col([dbc.Label("Analyze Stats")],width=1),
+                dbc.Col([dcc.Dropdown(id = 'DispStats',
                         options=[
                             {'label': i, 'value': i} for i in statscolumns],
                         multi=True
                         ,value=["POS ADP Y2"]
                         ,className="dash-bootstrap"
-                    ),
+                    )],width=11),
+        ])
                 
                 ]),
         html.Div(id="emptydiv")
@@ -139,10 +152,15 @@ html.Div(id="wrapper",children=[dbc.Row([
 @app.callback(
     [Output(component_id='Season', component_property='options'),
      Output(component_id='Season', component_property='value')], # or is there a different one for table
-    [Input(component_id='Player', component_property='value')])
+    [Input(component_id='Player', component_property='value')],
+    [State(component_id='Season', component_property='value')])
     
-def UpdateDrop (Player):
-    return [{'label': i, 'value': i} for i in sorted(ADP['Year'][ADP.Player==Player].unique())],None
+def UpdateDrop (Player,Season):
+    opts=sorted(ADP['Year'][ADP.Player==Player].unique())
+    if Season in opts:
+        return [{'label': i, 'value': i} for i in opts],Season
+    else:
+        return [{'label': i, 'value': i} for i in opts],None
 
 # call back that limits model options to requisite make
 @app.callback(
@@ -196,11 +214,46 @@ def dashtable ( Player,Season,stats,dispstats):
         NEWx=bballtrial[scaledstats]
         arr=NEWx.values # converts from df to array 
         print(PlayerData)
-        bballtrial["Euc"]=euclidean_distances(y,arr)[0] #assign list of euclidean distances to a new column, list must be exactly the same length as the df
-        bballtrial=bballtrial.sort_values("Euc").reset_index(drop=True)
-        bballtrial=bballtrial[bballtrial["Player"]!=Player]
-        bballtrial = bballtrial.iloc[0:10]
-        bballtrial=bballtrial[['Player','Year',"POS"]+stats+dispstats]
+        try:
+            bballtrial["Euc"]=euclidean_distances(y,arr)[0] #assign list of euclidean distances to a new column, list must be exactly the same length as the df
+            bballtrial=bballtrial.sort_values("Euc").reset_index(drop=True)
+            bballtrial=bballtrial[bballtrial["Player"]!=Player]
+            bballtrial = bballtrial.iloc[0:10]
+            bballtrial=bballtrial[['Player','Year',"POS"]+stats+dispstats]
+        except:
+            PlayerData=PlayerData[['Player','Year',"POS"]+stats]
+            return [dt.DataTable(
+                id='Ctable',
+                columns=[{"name": i, "id": i} 
+                         for i in PlayerData.columns],
+                data=PlayerData.to_dict('records'),
+                fixed_rows={'headers': True},
+                fixed_columns={'headers': True},
+                style_header={
+             'fontSize':14,
+            'fontFamily': 'helvetica',
+            'border': 'thin #a5d4d9 solid',
+            'color': '#a5d4d9',
+            'backgroundColor': '#313131',
+            'padding':'10px'
+            },
+            style_filter={'color': '#fff', "backgroundColor": "#313131"},
+            style_table={'minHeight':'auto','height': 'auto','maxHeight':'auto','border': '#000','height': 'auto',"width":"95%"},
+            style_data={'whiteSpace': 'pre-line'},
+            style_cell={
+            'fontSize':12,
+            'border': 'thin #a5d4d9 solid',
+            'fontFamily': 'helvetica',
+            'textAlign': 'left',
+            'Width': 'auto',
+            'maxWidth': 0,
+            'height': 'auto',
+            'whiteSpace': 'normal',
+            'padding':'10px',
+            'color': '#a5d4d9',
+            'backgroundColor': '#313131'
+            }),html.H1("Error, Check that Comparison Stats are present for this Player/Season")]
+        
         arr=NEWx.values 
         PlayerData=PlayerData[['Player','Year',"POS"]+stats+dispstats]
         # print(len(bballtrial))
@@ -220,7 +273,7 @@ def dashtable ( Player,Season,stats,dispstats):
             'padding':'10px'
             },
         style_filter={'color': '#fff', "backgroundColor": "#313131"},
-        style_table={'minHeight':'auto','height': 'auto','maxHeight':'auto','border': '#000','height': '650px',"width":"95%"},
+        style_table={'minHeight':'auto','height': 'auto','maxHeight':'auto','border': '#000','height': 'auto',"width":"95%"},
         style_data={'whiteSpace': 'pre-line'},
         style_cell={
         'fontSize':12,
@@ -248,7 +301,7 @@ def dashtable ( Player,Season,stats,dispstats):
             'padding':'10px'
             },
         style_filter={'color': '#fff', "backgroundColor": "#313131"},
-        style_table={'minHeight':'auto','height': 'auto','maxHeight':'auto','border': '#000','height': '650px',"width":"95%"},
+        style_table={'minHeight':'auto','height': 'auto','maxHeight':'auto','border': '#000','height': 'auto',"width":"95%"},
         style_data={'whiteSpace': 'pre-line'},
         style_cell={
         'fontSize':12,

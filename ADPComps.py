d2d1c8d3116803adb75eccabb11e4714c7ede292
Carlo Surace <carlosurace@gmail.com>
2021-07-29 17:35:08 -0500

    Update

--- ADPComps.py
+++ ADPComps.py
@@ -50,9 +50,12 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 #load data
 ADP= pd.read_csv(os.path.join(THIS_FOLDER,"data/ADP.csv"))
 
-statscolumns = [col for col in ADP.columns]
+statscolumns = [col for col in ADP.columns if col not in ["Year in League","Age Entering Y1"]]
 
+logcols=[col for col in ADP.columns if "ADP" in col or "Finish" in col]
 
+for col in logcols:
+    ADP[col]=np.log(ADP[col])
 # dash lay out
 
 
@@ -85,21 +88,12 @@ ADPComps =html.Div([
                     )],width=11),
         ]),
         dbc.Row([
-            dbc.Col([dbc.Label("Year(Y1)")],width=1),
-                dbc.Col([dcc.Dropdown(id = 'Season',value=None
-                        ,options=[
-                            {'label': i, 'value': i} for i in sorted(ADP.Year.unique())],
-                        multi=False
-                        ,className="dash-bootstrap"
-                    )],width=11),
-        ]),
-        dbc.Row([
             dbc.Col([dbc.Label("Comparison Stats")],width=1),
                 dbc.Col([dcc.Dropdown(id = 'CompStats',
                         options=[
                             {'label': i, 'value': i} for i in statscolumns],
                         multi=True
-                        ,value=["POS.ADP","Y1 Finish"]
+                        ,value=["POS.ADP","Prior Season PPG"]
                         ,className="dash-bootstrap"
                     )],width=11),
         ]),
@@ -109,10 +103,29 @@ ADPComps =html.Div([
                         options=[
                             {'label': i, 'value': i} for i in statscolumns],
                         multi=True
-                        ,value=["POS ADP Y2"]
+                        ,value=["POS ADP Y2","Y1 PPG"]
                         ,className="dash-bootstrap"
                     )],width=11),
-        ])
+        ]),
+        dbc.Row([
+            dbc.Col([dbc.Label("Minimum Y1 Games")],width=1),
+        
+                dbc.Col([dcc.Dropdown(id = 'MinGames',value=13
+                        ,options=[
+                            {'label': i, 'value': i} for i in range(0,17)],
+                        multi=False
+                        ,className="dash-bootstrap"
+                    )],width=11),
+        ]),
+        dbc.RadioItems(id="match",
+                        options=[
+                            {'label': 'Comps Must Match Age', 'value': 'Age'},
+                            {'label': 'Comps Must Match Experience', 'value': 'Experience'},
+                            {'label': 'Comps Must Match Both', 'value': 'Both'},
+                            
+                            
+                        ],value="Experience",style={'color':'#fff','margin':'10px'},
+            inputStyle={"color": "#ccff00"})
                 
                 ]),
         html.Div(id="emptydiv")
@@ -166,12 +179,13 @@ def UpdateDrop (Player,Season):
 @app.callback(
     Output(component_id='emptydiv', component_property='children'), # or is there a different one for table
     [Input(component_id='Player', component_property='value'),
-    Input(component_id='Season', component_property='value'),
     Input(component_id='CompStats', component_property='value'),
-    Input(component_id='DispStats', component_property='value'),    
+    Input(component_id='DispStats', component_property='value'),
+    Input(component_id='match', component_property='value'),
+    Input(component_id='MinGames', component_property='value'),    
     ])
     
-def dashtable ( Player,Season,stats,dispstats):
+def dashtable ( Player,stats,dispstats,match,min):
     '''
     if  Player and Season and stats:
         bballtrial=universe[universe.columns]
@@ -196,30 +210,43 @@ def dashtable ( Player,Season,stats,dispstats):
     )
     else:
     '''
-    if  Player and Season and stats:
+    if  Player and stats:
         bballtrial=ADP[ADP.columns]
         #statscolumns = list(df5.columns[3:20, 27:68]) # columns 26 thru 67, 73-75 3 THRU 20, 27 THRU 68
         scaledstats= ['s' + stat for stat in stats]
+        p=bballtrial[(bballtrial["Player"]==Player)&(bballtrial["Year"]==2021)].reset_index(drop=True)
+        POS=p["POS"].iloc[0]
         
+        bballtrial=bballtrial[(bballtrial["POS"]==POS)].reset_index(drop=True)
         min_max_scaler = preprocessing.MinMaxScaler()
         scaled = min_max_scaler.fit_transform(bballtrial[stats])
         #rename scaled data columns
         x=pd.DataFrame(scaled,columns=scaledstats) # was scaled stats before
-        bballtrial=pd.concat([bballtrial[['Player','Year',"POS"]+stats+dispstats],x],axis=1, ignore_index=False)
-        PlayerData = bballtrial[(bballtrial["Player"]==Player) & (bballtrial['Year']==Season)].reset_index(drop=True)
-        bballtrial=bballtrial[bballtrial["POS"]==PlayerData["POS"].iloc[0]]
+        bballtrial=pd.concat([bballtrial,x],axis=1, ignore_index=False)
+        PlayerData = bballtrial[(bballtrial["Player"]==Player)&(bballtrial["Year"]==2021)].reset_index(drop=True)
+        
+        PlayerData=PlayerData[['Player','Year',"POS","Year in League","Age Entering Y1"]+stats+dispstats+scaledstats]
+        print(PlayerData)
+        bballtrial=bballtrial[(bballtrial["Y1 Games"]>=min)].reset_index(drop=True)
+        if match =="Both" or match =="Experience":
+            bballtrial=bballtrial[(bballtrial["Year in League"]==PlayerData["Year in League"].iloc[0])].reset_index(drop=True)
+        if match =="Both" or match =="Age":
+            bballtrial=bballtrial[(bballtrial["Age Entering Y1"]==PlayerData["Age Entering Y1"].iloc[0])].reset_index(drop=True)
+        bballtrial=bballtrial[['Player','Year',"POS","Year in League","Age Entering Y1"]+stats+dispstats+scaledstats]
+        
+        
         #remove visual columns, leaving only scaled data
         y=PlayerData[scaledstats].iloc[0].values.reshape(1,-1)
         bballtrial=bballtrial.dropna(subset=scaledstats+dispstats)
         NEWx=bballtrial[scaledstats]
         arr=NEWx.values # converts from df to array 
-        print(PlayerData)
+        
         try:
             bballtrial["Euc"]=euclidean_distances(y,arr)[0] #assign list of euclidean distances to a new column, list must be exactly the same length as the df
             bballtrial=bballtrial.sort_values("Euc").reset_index(drop=True)
-            bballtrial=bballtrial[bballtrial["Player"]!=Player]
+            bballtrial=bballtrial[(bballtrial["Player"]!=Player)]
             bballtrial = bballtrial.iloc[0:10]
-            bballtrial=bballtrial[['Player','Year',"POS"]+stats+dispstats]
+            bballtrial=bballtrial[['Player','Year',"POS","Year in League","Age Entering Y1"]+stats+dispstats]
         except:
             PlayerData=PlayerData[['Player','Year',"POS"]+stats]
             return [dt.DataTable(
@@ -255,7 +282,13 @@ def dashtable ( Player,Season,stats,dispstats):
             }),html.H1("Error, Check that Comparison Stats are present for this Player/Season")]
         
         arr=NEWx.values 
-        PlayerData=PlayerData[['Player','Year',"POS"]+stats+dispstats]
+        PlayerData=PlayerData[['Player','Year',"POS","Year in League","Age Entering Y1"]+stats+dispstats]
+        templogcols=[col for col in bballtrial.columns if "ADP" in col or "Finish" in col]
+        for col in templogcols:
+            PlayerData[col]=np.exp(PlayerData[col])
+            PlayerData[col]=PlayerData[col].round(0)
+            bballtrial[col]=np.exp(bballtrial[col])
+            bballtrial[col]=bballtrial[col].round(0)
         # print(len(bballtrial))
         return [dt.DataTable(
                 id='Ctable',
@@ -287,7 +320,7 @@ def dashtable ( Player,Season,stats,dispstats):
         'padding':'10px',
         'color': '#a5d4d9',
         'backgroundColor': '#313131'
-        }),
+        }),html.Br(),
         dt.DataTable(
         id='Euc return table',
         columns=[{"name": i, "id": i} for i in bballtrial.columns],
@@ -316,4 +349,5 @@ def dashtable ( Player,Season,stats,dispstats):
         'color': '#a5d4d9',
         'backgroundColor': '#313131'
         },
-        )]+[html.H1(cstat+": "+str(bballtrial[cstat].mean())) for cstat in dispstats]
+        )]+[dbc.Row([dbc.Col([html.H1("Comp Summary")]+[html.H2(cstat+": "+str(round(bballtrial[cstat].mean(),2))+" vs "+str(round(PlayerData[cstat].iloc[0],2))) for cstat in stats],width=6),
+                    dbc.Col([html.H1("Comp Averages")]+[html.H2(cstat+": "+str(round(bballtrial[cstat].mean(),2))) for cstat in dispstats],width=6)])]
